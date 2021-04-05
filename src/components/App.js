1b4b99f65d8ea912f2f50b08a75bb8505a6760e0
Adam Schwartz <adamberard24@gmail.com>
2021-04-05 16:16:30 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,20 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <p>MY NAME IS ADAM AND THIS IS ABOUT ME BABY!!!</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About/>
     </div>
   );
 }
